b47e1997326aa9b2ee64ddeb1fdb3036d63a11bd
Mayuri Sharma <sharmamayuri98@gmail.com>
2021-01-20 16:15:30 -0500

    updated the menu-item to navigate withRouter HOC

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,7 +1,7 @@
-import React from "react";
-import MainMenu from "../../components/main-menu/main-menu.component";
+import React from 'react';
+import MainMenu from '../../components/main-menu/main-menu.component';
 
-import "./homepage.styles.scss";
+import './homepage.styles.scss';
 
 const HomePage = () => (
     <div className="homepage">
